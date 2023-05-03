e1d4bea923cf636c6f331280e24eb48b46b382e7
김진영 <jinyoung01099@gmail.com>
2023-05-03 19:46:31 +0900

    [Add] post-item 컴포넌트 구현

--- components/posts/post-item.tsx
+++ components/posts/post-item.tsx
@@ -1,9 +1,38 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { PostType } from "./posts-grid";
+import classes from "./post-item.module.css";
+
 interface Props {
-  posts: any;
+  post: PostType;
 }
 
-const PostItem = () => {
-  return <></>;
+const PostItem: React.FC<Props> = ({ post }) => {
+  const { title, image, excerpt, date, slug } = post;
+
+  const formattedDate = new Date(date).toLocaleDateString("ko-KR", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const imagePath = `/images/posts/${slug}/${image}`;
+
+  return (
+    <li className={classes.post}>
+      <Link href="/">
+        <div className={classes.image}>
+          <Image src={imagePath} alt={title} width={300} height={200} />
+        </div>
+        <div className={classes.content}>
+          <h3>TITLE</h3>
+          <time>{formattedDate}</time>
+          <p>{excerpt}</p>
+        </div>
+      </Link>
+    </li>
+  );
 };
 
 export default PostItem;
--- components/posts/posts-grid.tsx
+++ components/posts/posts-grid.tsx
@@ -1,15 +1,23 @@
 import PostItem from "./post-item";
 import classes from "./posts-grid.module.css";
 
+export interface PostType {
+  title: string;
+  image: string;
+  excerpt: string;
+  date: string;
+  slug: string;
+}
+
 interface Props {
-  posts: any;
+  posts: PostType[];
 }
 
 const PostsGrid: React.FC<Props> = ({ posts }) => {
   return (
     <ul className={classes.grid}>
-      {posts.map((post: any) => (
-        <PostItem />
+      {posts.map((post: PostType) => (
+        <PostItem post={post} />
       ))}
     </ul>
   );
