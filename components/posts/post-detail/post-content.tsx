908fb104311ded040f3f0f59fd2b840255480026
김진영 <jinyoung01099@gmail.com>
2023-05-06 18:16:13 +0900

    [Add] markdown 이미지 next image로 대체

--- components/posts/post-detail/post-content.tsx
+++ components/posts/post-detail/post-content.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 
 import PostHeader from "./post-header";
@@ -7,13 +8,29 @@ import { PostType } from "@/pages";
 interface Props {
   post: PostType;
 }
+
 const PostContent: React.FC<Props> = ({ post }) => {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    img(image: any) {
+      return (
+        <Image
+          src={`/images/posts/${post.slug}/${image.src}`}
+          alt={image.alt}
+          width={900}
+          height={450}
+        />
+      );
+    },
+  };
 
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />{" "}
-      <ReactMarkdown>{post.content!}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>
+        {post.content!}
+      </ReactMarkdown>
     </article>
   );
 };
