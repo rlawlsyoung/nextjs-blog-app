db18fd2ea1a34bb712f02cd77c470364b8b027cb
김진영 <jinyoung01099@gmail.com>
2023-05-08 15:58:04 +0900

    [Add] 클라이언트에서 API 라우트로 contact 전송 기능 추가

--- components/contact/contact-form.tsx
+++ components/contact/contact-form.tsx
@@ -1,23 +1,47 @@
+import { useRef } from "react";
+
 import classes from "./contact-form.module.css";
 
 const ContactForm = () => {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const nameRef = useRef<HTMLInputElement>(null);
+  const messageRef = useRef<HTMLTextAreaElement>(null);
+
+  const sendMessageHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const newMessage = {
+      email: emailRef.current?.value,
+      name: nameRef.current?.value,
+      message: messageRef.current?.value,
+    };
+
+    fetch("/api/contact", {
+      method: "POST",
+      body: JSON.stringify(newMessage),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
+
   return (
     <section className={classes.contact}>
       <h1>How can I help you?</h1>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={sendMessageHandler}>
         <div className={classes.controls}>
           <div className={classes.control}>
             <label htmlFor="email">Your Email</label>
-            <input type="email" id="email" required />
+            <input type="email" id="email" required ref={emailRef} />
           </div>
           <div className={classes.control}>
             <label htmlFor="name">Your Name</label>
-            <input type="text" id="name" required />
+            <input type="text" id="name" required ref={nameRef} />
           </div>
         </div>
         <div className={classes.control}>
           <label htmlFor="message">Your Message</label>
-          <textarea id="message" rows={5}></textarea>
+          <textarea id="message" rows={5} ref={messageRef} required></textarea>
         </div>
         <div className={classes.actions}>
           <button>Send Message</button>
